ce78a35f641a5ebe6059ae9d042fec5637775420
Dmytriy Kravhenko <andreich1@bigmir.net>
2020-04-22 22:36:12 +0300

    chenge task-3

--- js/task-3.js
+++ js/task-3.js
@@ -10,14 +10,14 @@ const makeTransaction = (transaction) => {
             const canProcess = Math.random() > 0.3;
 
             if (canProcess) {
-                resolve([ transaction.id, delay ]);
+                resolve({ id: transaction.id, time: delay });
             }
             reject(transaction.id);
         }, delay);
     });
 };
 
-const logSuccess = ([ id, time ]) => {
+const logSuccess = ({ id, time }) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
